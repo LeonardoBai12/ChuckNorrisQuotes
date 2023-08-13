4bb0a2654f113c905768ae4ae3757fa16d78f275
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2023-08-13 16:02:26 -0300

    Correçao CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+project.setProperty("mainClassName", "io.lb.chucknorrisquotes.presentation.MainActivityClass")
+
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
@@ -6,4 +7,9 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21"
     id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
     id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+subprojects {
+    apply(from = "${project.rootDir}/ktlint.gradle.kts")
 }
